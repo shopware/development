78e2e98d26069977f374245314a77af20b792752
Stephan Pohl <stp@shopware.com>
2018-05-03 13:42:00 +0200

    NEXT-250 Fixed assets path for installation in sub directories

--- dev-ops/administration/actions/build.sh
+++ dev-ops/administration/actions/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: build administration for production and run assetic
 
-npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ build
+npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ build -- __APP_URL__
 bin/console assets:install
