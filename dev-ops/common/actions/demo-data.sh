2b35b7fd5fde16b8cd22861631323fffcfdffd52
Oliver Skroblin <o.skroblin@shopware.com>
2018-05-29 14:43:04 +0200

    NEXT-318 - Add refresh dbal index call in demo data script

--- dev-ops/common/actions/demo-data.sh
+++ dev-ops/common/actions/demo-data.sh
@@ -2,3 +2,4 @@
 #DESCRIPTION: creates a demo data set
 
 bin/console framework:demodata --products=500 --categories=5 --manufacturers=25 -eprod --tenant-id=ffffffffffffffffffffffffffffffff
+bin/console dbal:refresh:index -eprod --tenant-id=ffffffffffffffffffffffffffffffff
