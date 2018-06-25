afa2d7a3f6590d1aa420668ff934cb8290132289
Jan Bücker <j.buecker@shopware.com>
2018-06-25 14:09:21 +0200

    Add remaining modules for phpstan

--- dev-ops/common/actions/init.sh
+++ dev-ops/common/actions/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#DESCRIPTION: Initialization of the SaaS project
+#DESCRIPTION: Install database, dependencies and create tenant with default data set
 
 # generate default SSL private/public key
 php dev-ops/generate_ssl.php
