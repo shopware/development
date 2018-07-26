9934292422bc3ac2d1a9b6cbedc3af6b60c3576d
Sebastian Franze <s.franze@shopware.com>
2018-07-26 09:13:10 +0200

    ntr - use template function of psh to copy docker.compose.orverride.yml; make tenant_id dynamically

--- dev-ops/common/actions/demo-data.sh
+++ dev-ops/common/actions/demo-data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: creates a demo data set
 
-bin/console framework:demodata --products=500 --categories=5 --manufacturers=25 -eprod --tenant-id=20080911ffff4fffafffffff19830531
-bin/console dbal:refresh:index -eprod --tenant-id=20080911ffff4fffafffffff19830531
+bin/console framework:demodata --products=500 --categories=5 --manufacturers=25 -eprod --tenant-id=__TENANT_ID__
+bin/console dbal:refresh:index -eprod --tenant-id=__TENANT_ID__
