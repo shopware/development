25cc0ae91f123cb56be0b831b835249633e60bf2
Jan Bücker <j.buecker@shopware.com>
2018-08-28 09:39:55 +0200

    Change E2E docker setup

--- dev-ops/administration/actions/e2e.sh
+++ dev-ops/administration/actions/e2e.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: runs e2e tests with nightwatch for the administration
 
-APP_URL=__APP_URL__ vendor/shopware/platform/src/Administration/Resources/administration/node_modules/nightwatch/bin/nightwatch --config vendor/shopware/platform/src/Administration/Resources/administration/test/e2e/nightwatch.conf.js --reporter junit
+NIGHTWATCH_HEADLESS=__NIGHTWATCH_HEADLESS__ APP_URL=__APP_URL__ vendor/shopware/platform/src/Administration/Resources/administration/node_modules/nightwatch/bin/nightwatch --config vendor/shopware/platform/src/Administration/Resources/administration/test/e2e/nightwatch.conf.js --reporter junit --env __NIGHTWATCH_ENV__
