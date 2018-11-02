aa1fbbf003463fdce214b9d8eae0d7bea6607564
Holger-Thomas Kaßner <h.kassner@shopware.com>
2018-11-02 14:33:25 +0100

    NTR - Fix escaping of numeric db names in shell scripts

--- dev-ops/common/actions/init-test-database.sh
+++ dev-ops/common/actions/init-test-database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mysql -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__ -e "DROP DATABASE IF EXISTS __DB_NAME___test"
-mysql -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__ -e "CREATE DATABASE __DB_NAME___test DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci"
+mysql -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__ -e "DROP DATABASE IF EXISTS \`__DB_NAME___test\`"
+mysql -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__ -e "CREATE DATABASE \`__DB_NAME___test\` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci"
 
 mysqldump __DB_NAME__ -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ | mysql __DB_NAME___test -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__
