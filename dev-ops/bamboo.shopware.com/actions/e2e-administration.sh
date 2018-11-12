2bbd0e12168fb9ec136114e29a9709820be0cfe6
Stephan Pohl <stp@shopware.com>
2018-11-12 16:30:35 +0100

    NEXT-921 Added install e2e dependency command for bamboo

--- dev-ops/bamboo.shopware.com/actions/e2e-administration.sh
+++ dev-ops/bamboo.shopware.com/actions/e2e-administration.sh
@@ -2,4 +2,5 @@
 
 INCLUDE: ./../../administration/actions/init.sh
 INCLUDE: ./../../administration/actions/build.sh
+INCLUDE: ./../../administration/actions/e2e-install.sh
 INCLUDE: ./../../administration/actions/e2e.sh
