93def2875d6b38ae3ec81401d0a9e4966afc47f6
Stephan Pohl <stp@shopware.com>
2018-11-12 13:55:55 +0100

    NEXT-921 Refactored the e2e commands

---
+++ dev-ops/administration/actions/e2e-install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#DESCRIPTION: installs the dependencies for the e2e tests using npm
+npm install --prefix vendor/shopware/platform/src/Administration/Resources/e2e/
--- dev-ops/administration/actions/e2e.sh
+++ dev-ops/administration/actions/e2e.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: runs e2e tests with nightwatch for the administration
 
-NIGHTWATCH_HEADLESS=__NIGHTWATCH_HEADLESS__ APP_URL=__APP_URL__ vendor/shopware/platform/src/Administration/Resources/administration/node_modules/nightwatch/bin/nightwatch --config vendor/shopware/platform/src/Administration/Resources/administration/test/e2e/nightwatch.conf.js --reporter junit --env __NIGHTWATCH_ENV__
+NIGHTWATCH_HEADLESS=__NIGHTWATCH_HEADLESS__ APP_URL=__APP_URL__ NIGHTWATCH_ENV=__NIGHTWATCH_ENV__ vendor/shopware/platform/src/Administration/Resources/e2e/node_modules/nightwatch/bin/nightwatch --config vendor/shopware/platform/src/Administration/Resources/e2e/runner-config.js --reporter junit --env __DOCKER_ENV__ __NIGHTWATCH_PARAMS__
