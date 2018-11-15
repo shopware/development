6e7712b2738113fa8c64e61b58417a7b18fc8d2a
Jan Bücker <j.buecker@shopware.com>
2018-11-15 13:19:31 +0100

    Change require to dev- instead of -dev

--- dev-ops/bamboo.shopware.com/actions/init-composer.sh
+++ dev-ops/bamboo.shopware.com/actions/init-composer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: initialization of your environment
 
-composer require "shopware/platform:__PLATFORM_BRANCH__-dev" --no-interaction --no-suggest --no-scripts
+composer require "shopware/platform:dev-__PLATFORM_BRANCH__" --no-interaction --no-suggest --no-scripts
