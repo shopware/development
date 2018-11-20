33ee70a60813de61bfacbaf15635ee238e63d441
Jan Pietrzyk <jp@shopware.com>
2018-11-20 16:11:06 +0100

    NEXT-1044 - implement feature tag settings

--- dev-ops/administration/actions/unit.sh
+++ dev-ops/administration/actions/unit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #DESCRIPTION: runs unit tests with karma for the administration
-APP_URL=__APP_URL__ CHROME_BIN=__CHROME_BIN__ BABEL_ENV=test TESTING_ENV=single npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ unit
+APP_URL=__APP_URL__ CHROME_BIN=__CHROME_BIN__ BABEL_ENV=test TESTING_ENV=single ENV_FILE=__ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ unit
