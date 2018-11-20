9947ffb592da10f1b2da9b8bb76ff01700512fa7
Jan Bücker <j.buecker@shopware.com>
2018-11-20 10:11:24 +0100

    Code style fixes

--- src/RequestBuilder.php
+++ src/RequestBuilder.php
@@ -9,8 +9,6 @@ use Shopware\Core\PlatformRequest;
 use Shopware\Storefront\StorefrontRequest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class RequestBuilder
 {
