2fbe9b5a31b7b7decfdfc21c1d4935f34acb437c
Stephan Pohl <stp@shopware.com>
2018-11-21 11:41:21 +0100

    NEXT-808 Added commands for the component library

---
+++ dev-ops/administration/actions/component-library-install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#DESCRIPTION: installs the dependencies for the component library tests using npm
+npm install --prefix vendor/shopware/platform/src/Administration/Resources/nuxt-component-library/
---
+++ dev-ops/administration/actions/component-library.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#DESCRIPTION: build the component library for production
+
+npm run --prefix vendor/shopware/platform/src/Administration/Resources/nuxt-component-library/ generate
