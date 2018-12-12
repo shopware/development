2c74d60e93f72a7d51d5ebef6491fbbad86145f6
Stephan Pohl <stp@shopware.com>
2018-12-12 15:15:33 +0100

    NEXT-1265 - Add dump command for feature flags

--- dev-ops/administration/actions/e2e.sh
+++ dev-ops/administration/actions/e2e.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #DESCRIPTION: runs e2e tests with nightwatch for the administration
 
+bin/console administration:dump:features
+
 NIGHTWATCH_HEADLESS=__NIGHTWATCH_HEADLESS__ APP_URL=__APP_URL__ NIGHTWATCH_ENV=__NIGHTWATCH_ENV__ vendor/shopware/platform/src/Administration/Resources/e2e/node_modules/nightwatch/bin/nightwatch --config vendor/shopware/platform/src/Administration/Resources/e2e/runner-config.js --reporter junit --env __E2E_ENV__ __NIGHTWATCH_PARAMS__
