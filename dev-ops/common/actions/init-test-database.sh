d79adc316e0c25023239a6d0936ae62dd697b273
Patrick Weyck <p.weyck@shopware.com>
2019-01-04 17:03:18 +0100

    Use psh variable for the e2e dump path and remove docker special case

--- dev-ops/common/actions/init-test-database.sh
+++ dev-ops/common/actions/init-test-database.sh
@@ -3,6 +3,6 @@
 mysql -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__ -e "DROP DATABASE IF EXISTS \`__DB_NAME___test\`"
 mysql -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__ -e "CREATE DATABASE \`__DB_NAME___test\` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci"
 
-mysqldump __DB_NAME__ -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ | mysql __DB_NAME___test -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__
+mysqldump __DB_NAME__ -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__ | mysql __DB_NAME___test -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__
 
 php bin/console cache:clear --env=test
--- dev-ops/e2e/actions/restore-db.sh
+++ dev-ops/e2e/actions/restore-db.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "__E2E_ENV__" = "default" ]; then (echo 'SET SESSION SQL_LOG_BIN=0;'; cat /tmp/e2e_dump.sql) | mysql -u __DB_USER__ -p__DB_PASSWORD__ -h 127.0.0.1 --port=4406 __DB_NAME__ < /tmp/e2e_dump.sql; else (echo 'SET SESSION SQL_LOG_BIN=0;'; cat /tmp/e2e_dump.sql) | mysql -u __DB_USER__ -p__DB_PASSWORD__ -h app_mysql --port=3306 __DB_NAME__; fi
+(echo 'SET SESSION SQL_LOG_BIN=0;'; cat '__E2E_RESTORE_DUMP_PATH__') | mysql -u __DB_USER__ -p__DB_PASSWORD__ -h __DB_HOST__ --port=__DB_PORT__ __DB_NAME__
