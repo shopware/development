62bfee8c7e9e54fa8d9c363eaa97d6e300b52d6f
Stephan Pohl <stp@shopware.com>
2019-01-08 13:48:17 +0100

    NTR - Add env to differentiate the different envs

--- dev-ops/administration/actions/watch.sh
+++ dev-ops/administration/actions/watch.sh
@@ -2,4 +2,4 @@
 #DESCRIPTION: starting administration dev server for hot module reloading. Disable linting with the parameter ESLINT_DISABLE=true
 
 bin/console administration:dump:plugins
-PORT=__DEVPORT__ HOST=__HOST__ ESLINT_DISABLE=__ESLINT_DISABLE__ ENV_FILE=__ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ dev -- __APP_URL__
+APP_ENV=__E2E_ENV__ PORT=__DEVPORT__ HOST=__HOST__ ESLINT_DISABLE=__ESLINT_DISABLE__ ENV_FILE=__ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ dev -- __APP_URL__
