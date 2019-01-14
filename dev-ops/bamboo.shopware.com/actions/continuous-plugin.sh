cca7fd4a2601e11772c73b196e57df4f867c7f01
Michael Telgmann <mt@shopware.com>
2019-01-14 16:17:16 +0100

    NEXT-1435 - Rename plugin refresh command

--- dev-ops/bamboo.shopware.com/actions/continuous-plugin.sh
+++ dev-ops/bamboo.shopware.com/actions/continuous-plugin.sh
@@ -2,7 +2,7 @@
 
 docker exec -u __USERKEY__ __APP_ID__ /usr/local/bin/wait-for-it.sh --timeout=60 mysql:3306
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:init --PLATFORM_BRANCH="master"
-docker exec -u __USERKEY__ __APP_ID__ php bin/console plugin:update
+docker exec -u __USERKEY__ __APP_ID__ php bin/console plugin:refresh
 docker exec -u __USERKEY__ __APP_ID__ php bin/console plugin:install --activate __PLUGIN__
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar init-test-database
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:unit-plugin --PLUGIN="__PLUGIN__"
--- dev-ops/common/actions/init-shopware.sh
+++ dev-ops/common/actions/init-shopware.sh
@@ -8,7 +8,7 @@ bin/console cache:clear --env=test
 bin/console database:migrate --all
 bin/console database:migrate-destructive --all
 
-bin/console plugin:update
+bin/console plugin:refresh
 
 bin/console rest:user:create admin --password=shopware
 
