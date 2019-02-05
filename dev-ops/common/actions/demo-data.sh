ccaab30f210e34369b33698fe9202d5dbf45e9a5
Jan Bücker <j.buecker@shopware.com>
2019-02-05 08:33:07 +0100

    Refactor demodata command

--- dev-ops/common/actions/demo-data.sh
+++ dev-ops/common/actions/demo-data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #DESCRIPTION: creates a demo data set
 
-APP_ENV=prod bin/console framework:demodata --products=500 --categories=5 --manufacturers=25
+APP_ENV=prod bin/console framework:demodata
 APP_ENV=prod bin/console dbal:refresh:index
 
 # clear cache for current environment
