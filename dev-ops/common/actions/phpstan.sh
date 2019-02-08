72d1c48b32ea9bda1256fae17f3ab9146593bc11
Jan Bücker <j.buecker@shopware.com>
2019-02-08 11:34:13 +0100

    Fix phpstan path when using platform as symlink

--- dev-ops/common/actions/phpstan.sh
+++ dev-ops/common/actions/phpstan.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: Run phpstan on core
 
-vendor/shopware/platform/bin/phpstan.phar analyze --level 5 --configuration vendor/shopware/platform/phpstan.neon vendor/shopware/platform/src src
+vendor/shopware/platform/bin/phpstan.phar analyze --level 5 --configuration vendor/shopware/platform/phpstan.neon $(realpath vendor/shopware/platform/src) src
