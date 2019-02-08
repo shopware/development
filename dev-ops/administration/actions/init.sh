7add82d348bfa957a52672e26b3aa972e27d792a
Jan Pietrzyk <jp@shopware.com>
2019-02-08 09:15:53 +0100

    NTR - use `clean-install` so the package-lock.json is used for node module install

--- dev-ops/administration/actions/init.sh
+++ dev-ops/administration/actions/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: init administration dependencies for docker setups
 
-npm install --prefix vendor/shopware/platform/src/Administration/Resources/administration/
+npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/administration/
--- dev-ops/e2e/actions/init.sh
+++ dev-ops/e2e/actions/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: installs the dependencies for the e2e tests using npm
 
-npm install --prefix vendor/shopware/platform/src/Administration/Resources/e2e/
+npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/e2e/
