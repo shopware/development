197ceceb8c425a63a354b157870226c53ed2946f
Dennis Garding <di@shopware.com>
2019-03-21 16:10:06 +0100

    NEXT-2310 - Add customer group to sales channel / fix tests

--- tests/RequestTransformerTest.php
+++ tests/RequestTransformerTest.php
@@ -255,6 +255,7 @@ class RequestTransformerTest extends TestCase
                 'paymentMethods' => [['id' => Defaults::PAYMENT_METHOD_DEBIT]],
                 'shippingMethods' => [['id' => Defaults::SHIPPING_METHOD]],
                 'countries' => [['id' => Defaults::COUNTRY]],
+                'customerGroupId' => Defaults::FALLBACK_CUSTOMER_GROUP
             ];
 
             return array_merge_recursive($defaults, $salesChannelData);
