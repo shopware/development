f324c1f45a4329eace4c2b907c3ce0f1a2d513f5
Jan Pietrzyk <jp@shopware.com>
2019-04-05 10:50:40 +0200

    NTR - update the build to remove and create dist directories

--- dev-ops/docs/scripts/render_puml.sh
+++ dev-ops/docs/scripts/render_puml.sh
@@ -10,5 +10,7 @@ for file in $(find $DOCS_DIR -name '*.puml' -print); do
     DEST_PATH=${DEST_DIR}/${DEST_NAME}.svg
 
     echo "RENDERING $file to ${DEST_PATH}"
+    rm -R $DEST_DIR
+    mkdir -p $DEST_DIR
     echo $(cat ${file} | docker run -i --rm shopware-plattform-plantuml -tsvg > ${DEST_PATH})
 done
