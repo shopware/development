2e23a78dea84fba3f8e334af9c08b783ea0ac5fd
Jan Pietrzyk <jp@shopware.com>
2019-04-09 15:25:52 +0200

    NTR - add

--- dev-ops/docs/scripts/render_puml.sh
+++ dev-ops/docs/scripts/render_puml.sh
@@ -8,9 +8,10 @@ for file in $(find $DOCS_DIR -name '*.puml' -print); do
     DEST_NAME=$(basename -s .puml $file)
     DEST_DIR=$(dirname $(dirname $file))/dist
     DEST_PATH=${DEST_DIR}/${DEST_NAME}.svg
+    DEST_PNG_PATH=${DEST_DIR}/${DEST_NAME}.png
 
-    echo "RENDERING $file to ${DEST_PATH}"
-    rm -R $DEST_DIR
+    echo "RENDERING $file to \n\t${DEST_PATH}\n\t${DEST_PNG_PATH}"
     mkdir -p $DEST_DIR
     echo $(cat ${file} | docker run -i --rm shopware-plattform-plantuml -tsvg > ${DEST_PATH})
+    echo $(cat ${file} | docker run -i --rm shopware-plattform-plantuml -tpng > ${DEST_PNG_PATH})
 done
