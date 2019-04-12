11716a4a62f093932b01579333649400515bd750
Michael Telgmann <mt@shopware.com>
2019-04-12 08:55:48 +0200

    NTR - Dump autoload file of plugin for unit test on bamboo

--- dev-ops/bamboo.shopware.com/actions/continuous-plugin.sh
+++ dev-ops/bamboo.shopware.com/actions/continuous-plugin.sh
@@ -2,7 +2,7 @@
 
 docker exec -u __USERKEY__ __APP_ID__ /usr/local/bin/wait-for-it.sh --timeout=60 mysql:3306
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:init
-docker exec -u __USERKEY__ __APP_ID__ php bin/console plugin:refresh
 docker exec -u __USERKEY__ __APP_ID__ php bin/console plugin:install --activate __PLUGIN__
+docker exec -u __USERKEY__ __APP_ID__ composer dump-autoload -d custom/plugins/__PLUGIN__
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar init-test-databases
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:unit-plugin --PLUGIN="__PLUGIN__"
