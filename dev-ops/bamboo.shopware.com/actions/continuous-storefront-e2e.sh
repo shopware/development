111254cc4381295ea4abe7e57a740e0ac01d9d9a
Ramona Schwering <R.Schwering@shopware.com>
2019-04-17 11:04:39 +0200

    NTR - Remove deprecated administration:e2e commands

--- dev-ops/bamboo.shopware.com/actions/continuous-storefront-e2e.sh
+++ dev-ops/bamboo.shopware.com/actions/continuous-storefront-e2e.sh
@@ -2,4 +2,4 @@
 
 docker exec -u __USERKEY__ __APP_ID__ /usr/local/bin/wait-for-it.sh --timeout=120 mysql:3306
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:init
-docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:e2e-storefront --NIGHTWATCH_ENV="storefront"
+docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:e2e-administration --NIGHTWATCH_ENV="storefront" --DB_NAME="shopware_e2e" --APP_ENV="prod"
