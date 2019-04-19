2ad00689fbce96992316ab15cdc4bdbbf215b838
Ramona Schwering <R.Schwering@shopware.com>
2019-04-19 18:43:37 +0200

    NEXT-1691 - Allow storefront usage in administration e2e tests

--- dev-ops/bamboo.shopware.com/actions/e2e-administration.sh
+++ dev-ops/bamboo.shopware.com/actions/e2e-administration.sh
@@ -2,5 +2,7 @@
 
 INCLUDE: ./../../administration/actions/init.sh
 INCLUDE: ./../../administration/actions/build.sh
+INCLUDE: ./../../storefront/actions/install.sh
+INCLUDE: ./../../storefront/actions/prod.sh
 INCLUDE: ./../../e2e/actions/init.sh
 INCLUDE: ./../../e2e/actions/run.sh
