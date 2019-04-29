d9406abbd710a3c3aacda4dc13dde23a5900433d
Jan Bücker <j.buecker@shopware.com>
2019-04-29 14:19:50 +0200

    Add backward compatibility

--- dev-ops/bamboo.shopware.com/actions/docs-jsdoc.sh
+++ dev-ops/bamboo.shopware.com/actions/docs-jsdoc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:init-composer
-docker exec -u __USERKEY__ __APP_ID__ ./psh.phar administration:init
+docker exec -u __USERKEY__ __APP_ID__ ./psh.phar administration:install-dependencies
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar administration:generate-api-docs
