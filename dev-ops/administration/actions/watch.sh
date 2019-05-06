7f412ce3105c61003925c60bba4c3ca50a5a34d7
Michael Telgmann <mt@shopware.com>
2019-05-06 10:55:06 +0200

    NTR - Update dependencies

--- dev-ops/administration/actions/watch.sh
+++ dev-ops/administration/actions/watch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: starting administration dev server for hot module reloading. Disable linting with the parameter ESLINT_DISABLE=true
 
-bin/console administration:dump:plugins
+bin/console administration:dump:bundles
 PROJECT_ROOT=__PROJECT_ROOT__ APP_ENV=__E2E_ENV__ PORT=__DEVPORT__ HOST=__HOST__ ESLINT_DISABLE=__ESLINT_DISABLE__ ENV_FILE=__PROJECT_ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ dev -- __APP_URL__
