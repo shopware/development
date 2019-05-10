f7b702fe18a763ced6353a7e34178138ee5734bd
Jan Pietrzyk <jp@shopware.com>
2019-05-10 10:07:36 +0200

    NTR - add commands to install plugins and fixup build

--- dev-ops/docs/actions/build.sh
+++ dev-ops/docs/actions/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t shopware-plattform-plantuml dev-ops/docs/docker/plantuml/.
+bin/console docs:dump-er
+bin/console docs:dump-core-characteristics
+bin/console docs:dump-platform-updates
 
+docker build -t shopware-plattform-plantuml dev-ops/docs/docker/plantuml/.
 sh ./dev-ops/docs/scripts/render_puml.sh __DOCS_DIR__
 
-#./psh.phar docker:start
-
-#docker exec -u __USERKEY__ __APP_ID__ ./psh.phar init
-#docker exec -u __USERKEY__ __APP_ID__ bin/console -p framework:schema platform/src/Docs/_new/3-api/dist/management-api.json
+bin/console docs:convert -i platform/src/Docs/_new/ -o build/docs -u /shopware-platform-dev
