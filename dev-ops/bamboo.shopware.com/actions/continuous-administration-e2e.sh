0edbcfcdccab82f7c8a6dccda40aae6ce4cb821c
Ramona Schwering <R.Schwering@shopware.com>
2019-05-18 19:52:52 +0200

    NEXT-3385 - Split admin and storefront tests

--- dev-ops/bamboo.shopware.com/actions/continuous-administration-e2e.sh
+++ dev-ops/bamboo.shopware.com/actions/continuous-administration-e2e.sh
@@ -3,5 +3,5 @@
 docker exec -u __USERKEY__ __APP_ID__ /usr/local/bin/wait-for-it.sh --timeout=120 mysql:3306
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:init
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:e2e-administration --CYPRESS_ENV="administration" --DB_NAME="shopware_e2e" --APP_ENV="prod"
-docker exec -u __USERKEY__ __CYPRESS_ID__ npm clean-install --prefix /e2e
-docker exec -u __USERKEY__ __CYPRESS_ID__ npx cypress run --project /e2e --browser chrome --spec /e2e/cypress/integration/administration/**/* --config baseUrl=__APP_URL__ || echo Exit code $?
+docker exec -u __USERKEY__ __CYPRESS_ID__ npm clean-install --prefix /e2e-administration
+docker exec -u __USERKEY__ __CYPRESS_ID__ npx cypress run --project /e2e-administration --browser chrome --config baseUrl=__APP_URL__ || echo Failures: $?
