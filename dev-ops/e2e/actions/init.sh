8ef52b95dc72883dc82b6b58ae5d0df54e62c0aa
Ramona Schwering <R.Schwering@shopware.com>
2019-05-18 21:30:17 +0200

    NEXT-3385 - Prepare init and run commands

--- dev-ops/e2e/actions/init.sh
+++ dev-ops/e2e/actions/init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: installs the dependencies for the e2e tests using npm in cypress container
 
-npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/
-CHROMEDRIVER_VERSION=LATEST CHROMEDRIVER_FORCE_DOWNLOAD=true npm run --prefix vendor/shopware/platform/src/Administration/Resources/ lerna bootstrap
-CHROMEDRIVER_VERSION=LATEST CHROMEDRIVER_FORCE_DOWNLOAD=true npm install --prefix vendor/shopware/platform/src/Administration/Resources/e2e chromedriver
+#./psh.phar e2e:start
+if [[ -z "__CYPRESS_ID__" ]]; then docker exec -u __USERKEY__ __CYPRESS_ID__ npm clean-install --prefix ./e2e; else npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/e2e/; fi
