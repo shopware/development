4d0036ab8b12469fcba944bf83498770c20a2cf9
Jan Pietrzyk <jp@shopware.com>
2019-06-05 10:25:19 +0200

    NTR - fix deploy actions

--- dev-ops/docs/actions/deploy.sh
+++ dev-ops/docs/actions/deploy.sh
@@ -2,4 +2,4 @@
 # Description: Generates all images and dumps converts the markdown files. Also deploys if this is possible
 
 INCLUDE: ./build.sh
-bin/console docs:convert -i platform/src/Docs/_new/ -o build/docs -u /shopware-platform-dev -s
+bin/console docs:convert -i platform/src/Docs/Resources/current/ -o build/docs -u /shopware-platform-dev -s
