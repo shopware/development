aea851c37e2684d0315f22c26401d62007c539f2
Jan Bücker <j.buecker@shopware.com>
2019-06-17 11:25:29 +0200

    NEXT-3508 Dump and copy storefront assets

--- dev-ops/administration/actions/install-dependencies.sh
+++ dev-ops/administration/actions/install-dependencies.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: installs the dependencies for the administration using npm
 
-npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/administration/
+npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources
+npm run --prefix vendor/shopware/platform/src/Administration/Resources lerna -- bootstrap
--- dev-ops/e2e/actions/init.sh
+++ dev-ops/e2e/actions/init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: installs the dependencies for the e2e tests using npm
 
-CHROMEDRIVER_VERSION=LATEST CHROMEDRIVER_FORCE_DOWNLOAD=true npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/e2e/
+npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/
+CHROMEDRIVER_VERSION=LATEST CHROMEDRIVER_FORCE_DOWNLOAD=true npm run --prefix vendor/shopware/platform/src/Administration/Resources/ lerna bootstrap
