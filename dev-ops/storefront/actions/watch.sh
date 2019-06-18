e27c35db8b252c2899fe485c1e87f60bdff702e1
Jens Küper <J.Kueper@shopware.com>
2019-06-18 11:45:16 +0200

    Fix dump plugin name in actions

--- dev-ops/storefront/actions/watch.sh
+++ dev-ops/storefront/actions/watch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: Starts the webpack watcher
 
-bin/console plugin:dump
+bin/console bundle:dump
 APP_URL=__APP_URL__ PROJECT_ROOT=__PROJECT_ROOT__/  npm --prefix vendor/shopware/platform/src/Storefront/Resources/ run watch
