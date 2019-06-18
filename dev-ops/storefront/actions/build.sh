79ef565751c1c573a8fcb880ff4bd32a46acf283
Stephan Pohl <stp@shopware.com>
2019-06-18 16:33:59 +0200

    NTR - Fix installation of the storefront dependencies and use the correct command to dump the bundles

--- dev-ops/storefront/actions/build.sh
+++ dev-ops/storefront/actions/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #DESCRIPTION: Builds the project for production
 
-bin/console plugin:dump
+bin/console bundle:dump
 PROJECT_ROOT=__PROJECT_ROOT__/  npm --prefix vendor/shopware/platform/src/Storefront/Resources/ run production
 bin/console assets:install
--- dev-ops/storefront/actions/install-dependencies.sh
+++ dev-ops/storefront/actions/install-dependencies.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #DESCRIPTION: Installs the node.js dependencies
 
+npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources
+npm run --prefix vendor/shopware/platform/src/Administration/Resources lerna -- bootstrap
 npm --prefix vendor/shopware/platform/src/Storefront/Resources/ clean-install
