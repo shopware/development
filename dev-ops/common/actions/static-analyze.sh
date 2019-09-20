cedb30f6a32a9afa6e147137815cd53eeea80e9c
Michael Telgmann <mt@shopware.com>
2019-09-20 13:46:00 +0200

    NTR - Update PHPStan

--- dev-ops/common/actions/static-analyze.sh
+++ dev-ops/common/actions/static-analyze.sh
@@ -3,4 +3,4 @@
 
 php dev-ops/analyze/phpstan-config-generator.php
 php dev-ops/analyze/vendor/bin/phpstan analyze --configuration vendor/shopware/platform/phpstan.neon
-php dev-ops/analyze/vendor/bin/psalm --config=vendor/shopware/platform/psalm.xml --threads=2 --show-info=false
+php dev-ops/analyze/vendor/bin/psalm --config=vendor/shopware/platform/psalm.xml --threads=4 --show-info=false
