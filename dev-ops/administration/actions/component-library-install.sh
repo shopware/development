73a922f046e378f18e9efc96a5e90a9cdadcb834
Oliver Skroblin <o.skroblin@shopware.com>
2019-10-28 15:15:34 +0100

    NEXT-4148 - Replaces Resources/administration references with Resources/app/administration

--- dev-ops/administration/actions/component-library-install.sh
+++ dev-ops/administration/actions/component-library-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: installs the dependencies for the component library tests using npm
 
-PROJECT_ROOT=__PROJECT_ROOT__ npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/administration/build/nuxt-component-library/
+PROJECT_ROOT=__PROJECT_ROOT__ npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/app/administration/build/nuxt-component-library/
