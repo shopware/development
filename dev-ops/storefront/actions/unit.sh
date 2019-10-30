bceb5e5383c9886da8e6360fc635f14f4fa20e35
Oliver Skroblin <o.skroblin@shopware.com>
2019-10-30 13:47:43 +0100

    NEXT-4148 - Added script for storefront unit tests

---
+++ dev-ops/storefront/actions/unit.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#DESCRIPTION: runs storefront unit tests
+
+PROJECT_ROOT=__PROJECT_ROOT__ npm --prefix vendor/shopware/platform/src/Storefront/Resources/app/storefront/ run unit
