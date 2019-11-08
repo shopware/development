c02a315e2fea99e6971f6f841298f813c36dc88c
Jannis Leifeld <j.leifeld@shopware.com>
2019-11-08 14:37:34 +0100

    NTR - add psh command for proxy server

---
+++ dev-ops/storefront/actions/hot-proxy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#DESCRIPTION: Starts the hot module reloading server
+
+bin/console theme:dump
+APP_URL=__APP_URL__ PROJECT_ROOT=__PROJECT_ROOT__/  npm --prefix vendor/shopware/platform/src/Storefront/Resources/app/storefront/ run-script hot-proxy
