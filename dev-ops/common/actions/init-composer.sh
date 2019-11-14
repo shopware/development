44150c03b688b9ca40e405e8c9e0a3724e19701a
erolschel <kontakt@erolschel.de>
2019-11-14 22:17:43 +0100

    install https://github.com/hirak/prestissimo to speed things up a bit

--- dev-ops/common/actions/init-composer.sh
+++ dev-ops/common/actions/init-composer.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+composer global require hirak/prestissimo
 composer install --no-interaction --optimize-autoloader --no-suggest --no-scripts
 composer install --no-interaction --optimize-autoloader --no-suggest --working-dir=dev-ops/analyze
