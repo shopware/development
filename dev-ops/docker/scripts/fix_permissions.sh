10d4e193b27998a47b2e2d9bec2ac0d5a65ffb0b
nils.schuch <schuch@nexus-netsoft.com>
2019-11-15 11:42:56 +0100

    WIN: - Fix permissions

--- dev-ops/docker/scripts/fix_permissions.sh
+++ dev-ops/docker/scripts/fix_permissions.sh
@@ -5,6 +5,6 @@ APP_CONTAINER=$(docker-compose ps -q app_server)
 docker exec -i ${APP_CONTAINER} /bin/sh -c 'chown -Rf application:application /app'
 docker exec -i ${APP_CONTAINER} /bin/sh -c 'chmod -R 0777 /app'
 docker exec -i ${APP_CONTAINER} /bin/sh -c 'chown -Rf application:application /.npm'
-docker exec -i ${APP_CONTAINER} /bin/sh -c 'chmod -R 0750 /.npm'
+docker exec -i ${APP_CONTAINER} /bin/sh -c 'chmod -R 0777 /.npm'
 docker exec -i ${APP_CONTAINER} /bin/sh -c 'chown -Rf application:application /.composer'
-docker exec -i ${APP_CONTAINER} /bin/sh -c 'chmod -R 0750 /.composer'
+docker exec -i ${APP_CONTAINER} /bin/sh -c 'chmod -R 0777 /.composer'
