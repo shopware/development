6cf7febc9c7a415fcc6cdcf337f3134905316423
Ramona Schwering <R.Schwering@shopware.com>
2019-11-19 08:42:01 +0100

    NTR - Fix e2e scripts

--- dev-ops/e2e/actions/init-local.sh
+++ dev-ops/e2e/actions/init-local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: installs the dependencies for the e2e tests using npm in cypress container
 
-npm clean-install --prefix vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/administration/test/e2e/;
+npm clean-install --prefix vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_FOLDER__/test/e2e/;
--- dev-ops/e2e/actions/open-docker.sh
+++ dev-ops/e2e/actions/open-docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ./psh.phar e2e:prepare-container;
-vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_ENV__/test/e2e/node_modules/.bin/cypress open --project ./vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_ENV__/test/e2e --config baseUrl=__APP_URL__ --env localUsage=__CYPRESS_LOCAL__,projectRoot=__PROJECT_ROOT__;
+
+vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_FOLDER__/test/e2e/node_modules/.bin/cypress open --project ./vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_FOLDER__/test/e2e --config baseUrl=__APP_URL__ --env localUsage=__CYPRESS_LOCAL__,projectRoot=__PROJECT_ROOT__;
