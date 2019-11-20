92684634e889f0a6bae03fa7f918ab80c76a68ee
Hans Höchtl <hhoechtl@1drop.de>
2019-11-20 11:37:57 +0100

    Add update script to be used post deployment

---
+++ dev-ops/common/actions/update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#DESCRIPTION: Update Shopware after deployment
+
+INCLUDE: ./cache.sh
+
+bin/console plugin:refresh
+
+bin/console database:migrate --all
+
+bin/console dal:refresh:index
+
+bin/console scheduled-task:register
+
+bin/console theme:refresh
