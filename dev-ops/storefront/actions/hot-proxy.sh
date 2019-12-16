11488cafd27e277e4e9109ffbc479ef324bdeb14
Sebastian Franze <s.franze@shopware.com>
2019-12-16 17:11:05 +0100

    NEXT-5954 - Make storefront proxy port configurable

--- dev-ops/storefront/actions/hot-proxy.sh
+++ dev-ops/storefront/actions/hot-proxy.sh
@@ -2,4 +2,4 @@
 #DESCRIPTION: Starts the hot module reloading server
 
 bin/console theme:dump
-APP_URL=__APP_URL__ PROJECT_ROOT=__PROJECT_ROOT__/ ESLINT_DISABLE=__ESLINT_DISABLE__ npm --prefix vendor/shopware/platform/src/Storefront/Resources/app/storefront/ run-script hot-proxy
+APP_URL=__APP_URL__ STOREFRONT_PROXY_PORT=__STOREFRONT_PROXY_PORT__ PROJECT_ROOT=__PROJECT_ROOT__/ ESLINT_DISABLE=__ESLINT_DISABLE__ npm --prefix vendor/shopware/platform/src/Storefront/Resources/app/storefront/ run-script hot-proxy
