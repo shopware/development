234845405e3f1265a0f5227b58054804ace1ef1f
Stephan Pohl <stp@shopware.com>
2020-01-07 09:08:35 +0100

    NEXT-6195 Fixes paths to the test suite

--- dev-ops/e2e/actions/open-docker.sh
+++ dev-ops/e2e/actions/open-docker.sh
@@ -2,4 +2,4 @@
 
 ./psh.phar e2e:prepare-container;
 
-vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_FOLDER__/test/e2e/node_modules/.bin/cypress open --project ./vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_FOLDER__/test/e2e --config baseUrl=__APP_URL__ --env localUsage=__CYPRESS_LOCAL__,projectRoot=__PROJECT_ROOT__;
+vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_FOLDER__/test/e2e/node_modules/@shopware-ag/e2e-testsuite-platform/node_modules/.bin/cypress open --project ./vendor/shopware/platform/src/__CYPRESS_ENV__/Resources/app/__CYPRESS_FOLDER__/test/e2e --config baseUrl=__APP_URL__ --env localUsage=__CYPRESS_LOCAL__,projectRoot=__PROJECT_ROOT__;
