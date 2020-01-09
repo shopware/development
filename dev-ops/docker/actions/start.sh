378a4e0f3d59e0eb14c9d4eb289e017ed172ea92
Tri Tran <t.tran@shopware.com>
2020-01-09 09:11:21 +0100

    Switch to use Docker Volumes, instead of Mount Binds

--- dev-ops/docker/actions/start.sh
+++ dev-ops/docker/actions/start.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-
 # check that the ~/.npm and ~/.composer folder are not owned by the root user
 dev-ops/docker/scripts/check_permissions.sh
 
-docker-compose build && docker-compose up -d
+# Start docker-sync
+if [[ -n "__DOCKER_SYNC_ENABLED__" ]]; then docker-sync start && echo "\n docker-sync is initially indexing files. It may take some minutes, until code changes take effect"; fi
 
+docker-compose build && docker-compose up -d
