385b17bf51d8c8c41e69a67ca684b79abd988936
Michael Telgmann <mt@shopware.com>
2020-01-13 10:49:37 +0100

    NTR - Update CS fixer tools

--- dev-ops/common/actions/fix-cs.sh
+++ dev-ops/common/actions/fix-cs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: fix code style in src folder
 
-if [ -e vendor/shopware/platform/easy-coding-standard.yml ]; then php dev-ops/analyze/vendor/bin/ecs check --fix vendor/shopware/platform/src --config vendor/shopware/platform/easy-coding-standard.yml; else php dev-ops/analyze/vendor/bin/php-cs-fixer fix -v --config=vendor/shopware/platform/.php_cs.dist; fi
+php dev-ops/analyze/vendor/bin/ecs check --fix vendor/shopware/platform/src --config vendor/shopware/platform/easy-coding-standard.yml
