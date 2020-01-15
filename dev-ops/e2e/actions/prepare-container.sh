62bfdc21eaabea91b5050409aa379f81c00784f9
Stephan Pohl <stp@shopware.com>
2020-01-15 13:51:20 +0100

    NEXT-6195 Fixes forever call in local docker setup

--- dev-ops/e2e/actions/prepare-container.sh
+++ dev-ops/e2e/actions/prepare-container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar e2e:dump-db;
-docker exec -u __USERKEY__ __APP_ID__ forever start platform/src/Administration/Resources/app/administration/test/e2e/routes/cypress.js;
+docker exec -u __USERKEY__ __APP_ID__ forever start vendor/shopware/platform/src/Administration/Resources/app/administration/test/e2e/node_modules/@shopware-ag/e2e-testsuite-platform/routes/cypress.js;
