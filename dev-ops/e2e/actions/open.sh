3d2949171e8743738a08d223f406611d452b65aa
Ramona Schwering <R.Schwering@shopware.com>
2020-02-11 12:42:53 +0100

    NTR - Improve e2e psh scripts

--- dev-ops/e2e/actions/open.sh
+++ dev-ops/e2e/actions/open.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-#DESCRIPTION: opens Cypress' e2e tests runner
-if [[ "__CYPRESS_LOCAL__" == "1" ]]; then bash ./dev-ops/e2e/scripts/open-local.sh __CYPRESS_ENV__ __APP_URL__ __PROJECT_ROOT__;  fi
+#DESCRIPTION: Opens Cypress' e2e tests runner
 
-if [[ -z "__CYPRESS_LOCAL__" ]]; then bash ./dev-ops/e2e/scripts/prepare-container.sh __USERKEY__ __APP_ID__; bash ./dev-ops/e2e/scripts/open-docker.sh __CYPRESS_ID__ __CYPRESS_ENV__ __APP_URL__;  fi
+if [ "__CYPRESS_LOCAL__" = "1" ]; then bash ./dev-ops/e2e/scripts/open-local.sh __CYPRESS_ENV__ __APP_URL__ __PROJECT_ROOT__;  fi
+
+if [ -z "__CYPRESS_LOCAL__" ]; then bash ./dev-ops/e2e/scripts/prepare-container.sh __USERKEY__ __APP_ID__; bash ./dev-ops/e2e/scripts/open-docker.sh __CYPRESS_ID__ __CYPRESS_ENV__ __APP_URL__;  fi
