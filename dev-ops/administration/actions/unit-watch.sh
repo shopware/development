2b5fb5d44bcd0a8f1c0fd842856b5b03ccf136a2
Stephan Pohl <s.pohl@shopware.com>
2020-02-12 14:04:08 +0100

    NEXT-5805 Adds admin path to jest test actions

--- dev-ops/administration/actions/unit-watch.sh
+++ dev-ops/administration/actions/unit-watch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: runs & auto watch files to re-run unit tests automatically on change
 
-PROJECT_ROOT=__PROJECT_ROOT__ APP_URL=__APP_URL__ CHROME_BIN=__CHROME_BIN__ BABEL_ENV=test TESTING_ENV=watch ENV_FILE=__PROJECT_ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/app/administration/ unit-watch
+PROJECT_ROOT=__PROJECT_ROOT__ ADMIN_PATH=__PROJECT_ROOT__/vendor/shopware/platform/src/Administration/Resources/app/administration APP_URL=__APP_URL__ CHROME_BIN=__CHROME_BIN__ BABEL_ENV=test TESTING_ENV=watch ENV_FILE=__PROJECT_ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/app/administration/ unit-watch
