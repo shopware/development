5813364db89ed807e8c8a45dadb33d8bb312f726
Tobias Berge <t.berge@shopware.com>
2020-02-20 10:48:00 +0100

    NEXT-5805 - Remove lerna from psh scripts

--- dev-ops/storefront/actions/install-dependencies.sh
+++ dev-ops/storefront/actions/install-dependencies.sh
@@ -2,6 +2,5 @@
 #DESCRIPTION: Installs the node.js dependencies
 
 npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources
-npm run --prefix vendor/shopware/platform/src/Administration/Resources lerna -- bootstrap
 npm --prefix vendor/shopware/platform/src/Storefront/Resources/app/storefront/ clean-install
 node vendor/shopware/platform/src/Storefront/Resources/app/storefront/copy-to-vendor.js
