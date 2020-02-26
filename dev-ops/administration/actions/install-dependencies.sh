a8ac0ae2907c1dd12e995c757953afe2a7ddc21c
Tobias Berge <t.berge@shopware.com>
2020-02-26 10:30:36 +0100

    NEXT-5805 - Fix npm install path

--- dev-ops/administration/actions/install-dependencies.sh
+++ dev-ops/administration/actions/install-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: installs the dependencies for the administration using npm
 
-npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources
+npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/app/administration
--- dev-ops/storefront/actions/install-dependencies.sh
+++ dev-ops/storefront/actions/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #DESCRIPTION: Installs the node.js dependencies
 
-npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources
+npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources/app/administration
 npm --prefix vendor/shopware/platform/src/Storefront/Resources/app/storefront/ clean-install
 node vendor/shopware/platform/src/Storefront/Resources/app/storefront/copy-to-vendor.js
