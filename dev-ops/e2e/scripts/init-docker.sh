223c35b5370ebe671de04552fda78a1a3a777bac
Ramona Schwering <R.Schwering@shopware.com>
2020-05-11 09:51:02 +0200

    NEXT-6822 - Update e2e:init and docker-compose

--- dev-ops/e2e/scripts/init-docker.sh
+++ dev-ops/e2e/scripts/init-docker.sh
@@ -9,6 +9,3 @@ docker exec -u "$USER_KEY" "$CONTAINER" ./psh.phar e2e:prepare-shopware
 
 # Prepare database dump
 docker exec -u "$USER_KEY" "$CONTAINER" ./psh.phar e2e:prepare-environment;
-
-# Start express server for docker usage
-docker exec -u "$USER_KEY" "$CONTAINER" forever start vendor/shopware/platform/src/Administration/Resources/app/administration/test/e2e/node_modules/@shopware-ag/e2e-testsuite-platform/routes/cypress.js;
