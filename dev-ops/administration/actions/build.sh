8236d7ee7d4e9d4eadad78f8658a7f7a2628d9ef
Jannis Leifeld <j.leifeld@shopware.com>
2020-05-19 08:28:15 +0200

    NEXT-6928 - change commands for new webpack single file components

--- dev-ops/administration/actions/build.sh
+++ dev-ops/administration/actions/build.sh
@@ -2,5 +2,5 @@
 #DESCRIPTION: build administration for production and run assetic
 
 bin/console bundle:dump
-PROJECT_ROOT=__PROJECT_ROOT__ npm run --prefix vendor/shopware/platform/src/Administration/Resources/app/administration/ build
+PROJECT_ROOT=__PROJECT_ROOT__ ENV_FILE=__PROJECT_ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/app/administration/ build
 bin/console assets:install
