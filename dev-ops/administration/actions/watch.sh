e9db1718f1c1d797ed597f088d6804c7a765f433
Stefan Sluiter <s.sluiter@shopware.com>
2020-08-25 10:47:51 +0200

    NEXT-9685 - new feature flag system feature dump for watcher

--- dev-ops/administration/actions/watch.sh
+++ dev-ops/administration/actions/watch.sh
@@ -2,4 +2,5 @@
 #DESCRIPTION: starting administration dev server for hot module reloading. Disable linting with the parameter ESLINT_DISABLE=true
 
 bin/console bundle:dump
+bin/console feature:dump
 PROJECT_ROOT=__PROJECT_ROOT__ PORT=__DEVPORT__ HOST=__HOST__ ESLINT_DISABLE=__ESLINT_DISABLE__ ENV_FILE=__PROJECT_ROOT__/.env APP_URL=__APP_URL__ npm run --prefix vendor/shopware/platform/src/Administration/Resources/app/administration/ dev
