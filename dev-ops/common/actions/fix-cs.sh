bed2ee0176fff13963f6cf0e089ba914eb677d39
Michael Telgmann <mt@shopware.com>
2020-10-02 10:23:38 +0200

    NEXT-11085 - Fix execution of code style fixer

--- dev-ops/common/actions/fix-cs.sh
+++ dev-ops/common/actions/fix-cs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: fix code style in src folder
 
-php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.php
-php dev-ops/analyze/vendor/bin/ecs check --fix src --config platform/easy-coding-standard.php
+if [ -f "platform/easy-coding-standard.php" ]; then php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.php; else php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.yml; fi;
+if [ -f "platform/easy-coding-standard.php" ]; then php dev-ops/analyze/vendor/bin/ecs check --fix src --config platform/easy-coding-standard.php; else php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.yml; fi;
--- dev-ops/common/actions/static-analyze.sh
+++ dev-ops/common/actions/static-analyze.sh
@@ -4,4 +4,4 @@
 php dev-ops/analyze/phpstan-config-generator.php
 php dev-ops/analyze/vendor/bin/phpstan analyze --autoload-file=dev-ops/analyze/vendor/autoload.php --configuration platform/phpstan.neon
 
-if [ -f "platform/psalm.new.xml" ]; then php dev-ops/analyze/vendor/bin/psalm --config=platform/psalm.new.xml --threads=2 --diff --show-info=false; else php dev-ops/analyze/vendor/bin/psalm --config=platform/psalm.xml --threads=2 --diff --show-info=false; fi;
+php dev-ops/analyze/vendor/bin/psalm --config=platform/psalm.xml --threads=2 --diff --show-info=false
