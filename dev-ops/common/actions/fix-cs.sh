736957bb5a155ba4a2ca5dc31d32dacc8c3b3eec
Michael Telgmann <mt@shopware.com>
2020-10-24 16:16:54 +0200

    NEXT-11614 - Add code style check and static analyse to pipeline

--- dev-ops/common/actions/fix-cs.sh
+++ dev-ops/common/actions/fix-cs.sh
@@ -2,4 +2,3 @@
 #DESCRIPTION: fix code style in src folder
 
 if [ -f "platform/easy-coding-standard.php" ]; then php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.php; else php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.yml; fi;
-if [ -f "platform/easy-coding-standard.php" ]; then php dev-ops/analyze/vendor/bin/ecs check --fix src --config platform/easy-coding-standard.php; else php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.yml; fi;
