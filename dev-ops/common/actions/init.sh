4b96b6f4d666d0507568ca95f6dc28d0f5eef131
Michael Telgmann <mt@shopware.com>
2021-02-12 14:41:00 +0100

    NEXT-13411 - Update of Psalm and PHPStan

--- dev-ops/common/actions/init.sh
+++ dev-ops/common/actions/init.sh
@@ -4,11 +4,7 @@
 # generate default SSL private/public key
 php dev-ops/generate_ssl.php
 
-rm -rf vendor/shopware
-rm -rf var/cache
-rm -rf dev-ops/analyze/vendor
-rm -rf composer.lock
-
+INCLUDE: ./cache.sh
 INCLUDE: ./init-composer.sh
 INCLUDE: ./init-database.sh
 INCLUDE: ./init-shopware.sh
