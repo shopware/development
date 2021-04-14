8c4c233e64ee4676cc62cf21cde1807a59d5e36c
Jannis Leifeld <j.leifeld@shopware.com>
2021-04-14 09:40:56 +0200

    NEXT-10086 - add commands for entity schema generation

--- dev-ops/administration/actions/unit.sh
+++ dev-ops/administration/actions/unit.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-#DESCRIPTION: runs unit tests with karma for the administration
+#DESCRIPTION: runs unit tests with jest for the administration
 
+INCLUDE: ./dump-entity-schema.sh
 PROJECT_ROOT=__PROJECT_ROOT__ ADMIN_PATH=__PROJECT_ROOT__/vendor/shopware/platform/src/Administration/Resources/app/administration APP_URL=__APP_URL__ CHROME_BIN=__CHROME_BIN__ BABEL_ENV=test TESTING_ENV=single ENV_FILE=__PROJECT_ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/app/administration/ unit
