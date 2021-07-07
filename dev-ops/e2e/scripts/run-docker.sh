e9f93790c4b1e50fa3b673dffe2a696115af0225
Patrick Weyck <p.weyck@shopware.com>
2021-07-07 16:19:11 +0200

    NEXT-14957 - Fix e2e scripts

--- dev-ops/e2e/scripts/run-docker.sh
+++ dev-ops/e2e/scripts/run-docker.sh
@@ -3,15 +3,17 @@
 # Constants
 CONTAINER=$1
 CYPRESS_ENV=$2
-APP_URL=$3
 CYPRESS_PARAMS=$4
 
-printf "\nCypress environment: ${CYPRESS_ENV}\n"
-printf "App-URL: ${APP_URL}\n\n"
+export CYPRESS_baseUrl=$3
+export CYPRESS_localUsage=false
 
-# Start express server for docker usage
-docker exec -u "$USER_KEY" "$CONTAINER" forever start vendor/shopware/platform/src/Administration/Resources/app/administration/test/e2e/node_modules/@shopware-ag/e2e-testsuite-platform/routes/cypress.js;
+printf "\nCypress environment: ${CYPRESS_ENV}\n"
+printf "App-URL: ${CYPRESS_baseUrl}\n\n"
 
 # Start Cypress in CLI
 printf "### Starting Cypress\n\n"
-docker exec -w /e2e-"$CYPRESS_ENV" "$CONTAINER" cypress run --config baseUrl="$APP_URL" $CYPRESS_PARAMS
+docker exec \
+    -e CYPRESS_baseUrl -e CYPRESS_localUsage \
+    -w /e2e-"$CYPRESS_ENV" "$CONTAINER" \
+    cypress run $CYPRESS_PARAMS
