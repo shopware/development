ac843df8e48a3f426851688fb8445edc26be98db
Patrick Weyck <p.weyck@shopware.com>
2021-07-09 08:38:43 +0200

    NEXT-14957 - Use bin/console e2e:dump-db

--- dev-ops/e2e/actions/dump-db.sh
+++ dev-ops/e2e/actions/dump-db.sh
@@ -2,4 +2,7 @@
 #DESCRIPTION: Creates backup of Shopware's database
 
 bin/console theme:change --all Storefront
+
+bin/console e2e:dump-db --env=e2e || true
+
 echo "SET unique_checks=0;SET foreign_key_checks=0;" | mysqldump -u '__DB_USER__' -p'__DB_PASSWORD__' -h '__DB_HOST__' --port='__DB_PORT__' -q --opt '__DB_NAME__' --no-autocommit --ignore-table '__DB_NAME__.enqueue' --ignore-table '__DB_NAME__.message_queue_stats' | gzip -9 > __E2E_RESTORE_DUMP_PATH__
