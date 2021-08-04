ed03608751d75ca7cc790a1fadfaf4344dfe9b7f
Timo Altholtmann <t.altholtmann@shopware.com>
2021-08-04 08:38:29 +0200

    NEXT-16184 - Add twig lint commands

---
+++ dev-ops/administration/actions/lint-twig.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#DESCRIPTION: runs twig lint on administration source
+
+PROJECT_ROOT=__PROJECT_ROOT__ APP_URL=__APP_URL__ CHROME_BIN=__CHROME_BIN__ BABEL_ENV=test TESTING_ENV=single ENV_FILE=__PROJECT_ROOT__/.env npm run --prefix vendor/shopware/platform/src/Administration/Resources/app/administration/ lint:twig
