9dffdea3c1d5a6ff9f0dc0fbd3d9f8b026b27538
Marcel Brode <m.brode@shopware.com>
2021-10-12 10:06:14 +0200

    NEXT-17603 - Fix psh fix-cs script

--- dev-ops/common/actions/fix-cs.sh
+++ dev-ops/common/actions/fix-cs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: fix code style in src folder
 
-if [ -e platform/ecs.php ]; then cd platform; if [ ! -r vendor/autoload.php ]; then composer update; fi; composer run ecs -- src/; fi
+if [ -e platform/ecs.php ]; then cd platform; if [ ! -r vendor/autoload.php ]; then composer update; fi; composer run ecs -- --fix src/; fi
 if [ ! -e platform/ecs.php ]; then php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.php; fi
