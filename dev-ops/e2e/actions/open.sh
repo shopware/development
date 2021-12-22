e5eae64a0cde4f3ebed4895174078debc28eefd2
Patrick Weyck <p.weyck@shopware.com>
2021-12-22 11:36:06 +0100

    NEXT-18582 - Change e2e commands to use new directory

--- dev-ops/e2e/actions/open.sh
+++ dev-ops/e2e/actions/open.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #DESCRIPTION: Opens Cypress' e2e tests runner
 
-if [ "__CYPRESS_LOCAL__" = "1" ]; then bash ./dev-ops/e2e/scripts/open-local.sh __CYPRESS_ENV__ __APP_URL__ __PROJECT_ROOT__;  fi
-if [ "__CYPRESS_LOCAL__" != "1" ]; then bash ./dev-ops/e2e/scripts/open-docker.sh __CYPRESS_ID__ __CYPRESS_ENV__ __APP_URL__;  fi
+if [ "__CYPRESS_LOCAL__" = "1" ]; then bash ./dev-ops/e2e/scripts/open-local.sh __APP_URL__ __PROJECT_ROOT__;  fi
+if [ "__CYPRESS_LOCAL__" != "1" ]; then bash ./dev-ops/e2e/scripts/open-docker.sh __CYPRESS_ID__ __APP_URL__;  fi
--- dev-ops/e2e/scripts/run-docker.sh
+++ dev-ops/e2e/scripts/run-docker.sh
@@ -2,18 +2,16 @@
 
 # Constants
 CONTAINER=$1
-CYPRESS_ENV=$2
-CYPRESS_PARAMS=$4
+export CYPRESS_baseUrl=$2
+CYPRESS_PARAMS=$3
 
-export CYPRESS_baseUrl=$3
 export CYPRESS_localUsage=false
 
-printf "\nCypress environment: ${CYPRESS_ENV}\n"
 printf "App-URL: ${CYPRESS_baseUrl}\n\n"
 
 # Start Cypress in CLI
 printf "### Starting Cypress\n\n"
 docker exec \
     -e CYPRESS_baseUrl -e CYPRESS_localUsage \
-    -w /e2e-"$CYPRESS_ENV" "$CONTAINER" \
+    -w /e2e "$CONTAINER" \
     cypress run $CYPRESS_PARAMS
